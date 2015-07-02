d8bcdff448c760b890b396aca6638d5d90fbd36f
David McClure <davidwilliammcclure@gmail.com>
2015-07-02 12:14:52 -0700

    In /people, wire up real data from the Person model.

--- app/controllers/v1/people_controller.rb
+++ app/controllers/v1/people_controller.rb
@@ -3,10 +3,7 @@ module V1
   class PeopleController < ApplicationController
 
     def index
-      @people = [
-        { family_name: "McClure", given_name: "David" },
-        { family_name: "Weisman", given_name: "Kara" }
-      ]
+      @people = Person.all
     end
 
   end
