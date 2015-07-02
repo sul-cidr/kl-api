3a3448754d8ef516b46a5e3d418cdd1881639709
David McClure <davidwilliammcclure@gmail.com>
2015-07-02 16:52:31 -0700

    Moves the header definition into a before(:each) block in the PeopleController tests.

--- spec/controllers/v1/people_controller_spec.rb
+++ spec/controllers/v1/people_controller_spec.rb
@@ -3,10 +3,13 @@ require 'rails_helper'
 
 describe V1::PeopleController, type: :controller do
 
+  before(:each) do
+    request.headers["Accept"] = "application/json; version=1"
+  end
+
   describe "GET #index" do
 
     it "provides a list of people" do
-      request.headers["Accept"] = "application/json; version=1"
       get :index
       # TODO
     end
