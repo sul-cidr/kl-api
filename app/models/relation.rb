91d7635926024d2637a0fcd28f867f529efedca0
David McClure <davidwilliammcclure@gmail.com>
2015-07-10 18:00:37 -0700

    Wire up ActiveRecord associations on the Relation model.

--- app/models/relation.rb
+++ app/models/relation.rb
@@ -1,2 +1,4 @@
 class Relation < ActiveRecord::Base
+  belongs_to :source, class_name: "Person", foreign_key: "source_id"
+  belongs_to :target, class_name: "Person", foreign_key: "target_id"
 end
--- spec/models/relation_spec.rb
+++ spec/models/relation_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 describe Relation, type: :model do
-  # TODO
+
+  describe "associatons" do
+
+    it do
+      should belong_to(:source)
+        .class_name("Person")
+        .with_foreign_key("source_id")
+    end
+
+    it do
+      should belong_to(:target)
+        .class_name("Person")
+        .with_foreign_key("target_id")
+    end
+
+  end
+
 end
