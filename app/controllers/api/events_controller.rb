02dd4d4b2a0f536e5c327c5ea598d6c75793c457
David McClure <davidwilliammcclure@gmail.com>
2015-07-14 14:51:52 -0700

    Spiking in filtering logic in the /events #index action.

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -3,7 +3,23 @@ module API
   class EventsController < ApplicationController
 
     def index
-      @events = paginate(Event.all)
+
+      events = Event.all
+
+      if params[:after]
+        events = events.after_year(params[:after])
+      end
+
+      if params[:before]
+        events = events.before_year(params[:before])
+      end
+
+      if params[:extent]
+        events = events.in_extent(params[:extent])
+      end
+
+      @events = paginate(events)
+
     end
 
   end
