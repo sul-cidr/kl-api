1beeb0c96102d571bce3cab815d99aa9b75c26a4
David McClure <davidwilliammcclure@gmail.com>
2015-07-15 09:55:31 -0700

    Implements /landmarks?extent and /landmarks?lon&lat&radius.

--- app/controllers/api/landmarks_controller.rb
+++ app/controllers/api/landmarks_controller.rb
@@ -3,7 +3,23 @@ module API
   class LandmarksController < ApplicationController
 
     def index
-      @landmarks = paginate(Landmark.all)
+
+      landmarks = Landmark.all
+
+      if params[:extent]
+        landmarks = landmarks.in_extent(params[:extent])
+      end
+
+      if params[:lon] and params[:lat] and params[:radius]
+        landmarks = landmarks.in_radius(
+          params[:lon].to_i,
+          params[:lat].to_i,
+          params[:radius],
+        )
+      end
+
+      @landmarks = paginate(landmarks)
+
     end
 
   end
