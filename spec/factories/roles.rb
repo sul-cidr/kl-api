c9e35eb893c1a72d1f5ce42e52f73ee776217039
David McClure <davidwilliammcclure@gmail.com>
2015-07-17 13:24:09 -0700

    Annotating Role.

--- spec/factories/roles.rb
+++ spec/factories/roles.rb
@@ -1,3 +1,11 @@
+# == Schema Information
+#
+# Table name: roles
+#
+#  id   :integer          not null, primary key
+#  name :string           not null
+#
+
 FactoryGirl.define do
   factory :role do
     
