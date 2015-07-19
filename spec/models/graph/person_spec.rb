524fa54dbb31101a9f4596dec0ed4ad9c7634925
David McClure <davidwilliammcclure@gmail.com>
2015-07-19 16:04:36 -0700

    Basic test for Graph::Person.kin.

--- spec/models/graph/person_spec.rb
+++ spec/models/graph/person_spec.rb
@@ -162,4 +162,33 @@ describe Graph::Person, :neo4j, :quiet do
 
   end
 
+  describe ".kin()" do
+
+    it "returns a path from source -> target" do
+
+      p1 = create(:person)
+      p2 = create(:person)
+      p3 = create(:person)
+      p4 = create(:person)
+      p5 = create(:person)
+
+      n1 = Graph::Person.add_node(p1.id)
+      n2 = Graph::Person.add_node(p2.id)
+      n3 = Graph::Person.add_node(p3.id)
+      n4 = Graph::Person.add_node(p4.id)
+      n5 = Graph::Person.add_node(p5.id)
+
+      n1.child << n2
+      n2.child << n3
+      n3.child << n4
+      n4.child << n5
+
+      path = Graph::Person.kin(p1.id, p5.id)
+
+      expect(path).to eq([p1, p2, p3, p4, p5])
+
+    end
+
+  end
+
 end
