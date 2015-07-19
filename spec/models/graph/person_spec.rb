c84bf4845c3151b4d8132d6dccc25afaf5b1e715
David McClure <davidwilliammcclure@gmail.com>
2015-07-19 14:43:56 -0700

    Sketching in a test for index_marriages().

--- spec/models/graph/person_spec.rb
+++ spec/models/graph/person_spec.rb
@@ -5,11 +5,30 @@ describe Graph::Person do
 
   describe ".index_marriages()" do
 
-    xit "creates nodes and relationships for marriages" do
+    it "creates nodes and relationships for marriages" do
+
       # create two events of type marriage
       # create 4 people, link to the events
       # index_marriages
       # check for the nodes
+
+      marriage = create(:event_type, name: "MARR")
+
+      e1 = create(:event, event_type: marriage)
+      e2 = create(:event, event_type: marriage)
+
+      p1 = create(:person)
+      p2 = create(:person)
+      p3 = create(:person)
+      p4 = create(:person)
+
+      create(:person_event, person: p1, event: e1)
+      create(:person_event, person: p2, event: e1)
+      create(:person_event, person: p3, event: e2)
+      create(:person_event, person: p4, event: e2)
+
+      Graph::Person.index_marriages
+
     end
 
   end
