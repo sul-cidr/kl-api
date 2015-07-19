eb7e23dff2dadd43f77422d244119c6e6a6e36c3
David McClure <davidwilliammcclure@gmail.com>
2015-07-19 16:55:41 -0700

    Tests the /relation endpoint.

--- app/controllers/api/relation_controller.rb
+++ app/controllers/api/relation_controller.rb
@@ -3,10 +3,16 @@ module API
   class RelationController < ApplicationController
 
     def index
-      @people = Graph::Person.kin(
+
+      ids = Graph::Person.kin(
         params[:source].to_i,
         params[:target].to_i
       )
+
+      @people = ids.map do |id|
+        Person.find(id)
+      end
+
     end
 
   end
---
+++ spec/controllers/api/relation_controller_spec.rb
@@ -0,0 +1,40 @@
+
+require 'rails_helper'
+
+describe API::RelationController, type: :controller do
+
+  render_views
+
+  before(:each) do
+    request.headers["Accept"] = "application/json"
+  end
+
+  describe "GET #index", :neo4j, :quiet do
+
+    it "returns a path from source -> target" do
+
+      p1 = create(:person)
+      p2 = create(:person)
+      p3 = create(:person)
+      p4 = create(:person)
+      p5 = create(:person)
+
+      n1 = Graph::Person.add_node(p1.id)
+      n2 = Graph::Person.add_node(p2.id)
+      n3 = Graph::Person.add_node(p3.id)
+      n4 = Graph::Person.add_node(p4.id)
+      n5 = Graph::Person.add_node(p5.id)
+
+      n1.child << n2
+      n2.child << n3
+      n3.child << n4
+      n4.child << n5
+
+      get :index, source: p1.id, target: p5.id
+      expect(response.body).to be_json_records(p1, p2, p3, p4, p5)
+
+    end
+
+  end
+
+end
