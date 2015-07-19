c827d3ae16784d53d0704775db9d0b86ddf96af9
David McClure <davidwilliammcclure@gmail.com>
2015-07-19 15:29:03 -0700

    Complete test for Graph::Person.index_marriages().

--- spec/models/graph/person_spec.rb
+++ spec/models/graph/person_spec.rb
@@ -11,11 +11,6 @@ describe Graph::Person, :neo4j, :quiet do
 
     it "creates nodes and relationships for marriages" do
 
-      # create two events of type marriage
-      # create 4 people, link to the events
-      # index_marriages
-      # check for the nodes
-
       marriage = create(:event_type, name: "MARR")
 
       e1 = create(:event, event_type: marriage)
@@ -25,6 +20,8 @@ describe Graph::Person, :neo4j, :quiet do
       p2 = create(:person)
       p3 = create(:person)
       p4 = create(:person)
+      p5 = create(:person)
+      p6 = create(:person)
 
       create(:person_event, person: p1, event: e1)
       create(:person_event, person: p2, event: e1)
@@ -32,6 +29,25 @@ describe Graph::Person, :neo4j, :quiet do
       create(:person_event, person: p4, event: e2)
 
       Graph::Person.index_marriages
+
+      p1_node = Graph::Person.find_by(pg_id: p1.id)
+      p2_node = Graph::Person.find_by(pg_id: p2.id)
+      p3_node = Graph::Person.find_by(pg_id: p3.id)
+      p4_node = Graph::Person.find_by(pg_id: p4.id)
+      p5_node = Graph::Person.find_by(pg_id: p5.id)
+      p6_node = Graph::Person.find_by(pg_id: p6.id)
+
+      # Should create nodes.
+      expect(p1_node).to_not be_nil
+      expect(p2_node).to_not be_nil
+      expect(p3_node).to_not be_nil
+      expect(p4_node).to_not be_nil
+      expect(p5_node).to be_nil
+      expect(p6_node).to be_nil
+
+      # Should create relationships.
+      expect(p1_node.spouse).to eq(p2_node)
+      expect(p1_node.spouse).to eq(p2_node)
 
     end
 
