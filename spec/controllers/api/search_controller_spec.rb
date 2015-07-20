83252bcea1670f07bef405ea2055fa8d50d0ae7a
David McClure <davidwilliammcclure@gmail.com>
2015-07-20 15:39:19 -0700

    Adds a failing test for a /search endpoint that returns records 'binned' by type.

--- spec/controllers/api/search_controller_spec.rb
+++ spec/controllers/api/search_controller_spec.rb
@@ -13,19 +13,27 @@ describe API::SearchController, type: :controller do
 
     it "searches people, landmarks, and occupations" do
 
-      p1 = create(:person, given_name: "kindred")
-      create(:person, given_name: "britain")
+      p1 = create(:person, given_name: "dog")
+      p2 = create(:person, given_name: "dog")
+      create(:person, given_name: "cat")
 
-      l1 = create(:landmark, name: "kindred")
-      create(:landmark, name: "britain")
+      l1 = create(:landmark, name: "dog")
+      l2 = create(:landmark, name: "dog")
+      create(:landmark, name: "cat")
 
-      o1 = create(:occupation, name: "kindred")
-      create(:occupation, name: "britain")
+      o1 = create(:occupation, name: "dog")
+      o2 = create(:occupation, name: "dog")
+      create(:occupation, name: "cat")
 
       Sunspot.commit
 
-      get :index, query: "kindred"
-      expect(response.body).to be_solr_records(p1, l1, o1)
+      get :index, query: "dog"
+
+      json = JSON.parse(response.body)
+
+      expect(json["people"]).to be_solr_records(p1, p2)
+      expect(json["landmarks"]).to be_solr_records(l1, l2)
+      expect(json["occupations"]).to be_solr_records(o1, o2)
 
     end
 
