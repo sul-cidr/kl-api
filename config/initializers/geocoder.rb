f2e040db3712d78a624b766365ecc2030ae3df75
David McClure <davidwilliammcclure@gmail.com>
2015-07-27 15:02:47 -0700

    Use the Data Science Toolkit geocoder, with ENV-ified host.

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -19,6 +19,7 @@ Geocoder.configure(
   # :units     => :mi,       # :km for kilometers or :mi for miles
   # :distances => :linear    # :spherical or :linear
 
-  :lookup => :nominatim
+  :lookup => :dstk,
+  :host => ENV["DSTK_HOST"]
 
 )
