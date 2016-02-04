5655851242e9bca3987324e1f28b794355bfb045
David McClure <davidwilliammcclure@gmail.com>
2016-02-04 14:55:16 -0800

    Update field annotations.

--- app/models/photograph.rb
+++ app/models/photograph.rb
@@ -2,15 +2,20 @@
 #
 # Table name: photographs
 #
-#  id         :integer          not null, primary key
-#  lonlat     :geometry({:srid= point, 0
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  flickr_id  :integer          not null
-#  url        :string           not null
-#  title      :string
-#  address    :string
-#  geocode    :boolean          default(FALSE), not null
+#  id                   :integer          not null, primary key
+#  lonlat               :geometry({:srid= point, 0
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
+#  flickr_id            :integer          not null
+#  url                  :string           not null
+#  title                :string
+#  address              :string
+#  needs_coord          :boolean          default(FALSE), not null
+#  license              :integer
+#  copyright_permission :boolean
+#  fee                  :string
+#  credit               :string
+#  accuracy             :integer
 #
 
 class Photograph < ActiveRecord::Base
--- spec/factories/landmarks.rb
+++ spec/factories/landmarks.rb
@@ -2,12 +2,12 @@
 #
 # Table name: landmarks
 #
-#  id            :integer          not null, primary key
-#  name          :string
-#  created_at    :datetime         not null
-#  updated_at    :datetime         not null
-#  lonlat        :geometry({:srid= point, 0
-#  unveiled_year :integer
+#  id         :integer          not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  lonlat     :geometry({:srid= point, 0
+#  start_year :integer
 #
 
 FactoryGirl.define do
