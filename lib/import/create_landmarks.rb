230dbc8caea45e74207db14537cdaa402764a877
David McClure <davidwilliammcclure@gmail.com>
2016-02-04 14:25:46 -0800

    Use the csv() helper on the base step class in CreateLandmarks, instead of manually loading the file.

--- lib/import/create_landmarks.rb
+++ lib/import/create_landmarks.rb
@@ -3,11 +3,7 @@ module Import
   class CreateLandmarks < Step
 
     def up
-
-      # Form the CSV path.
-      path = Rails.root.join('data/landmarks.csv')
-
-      CSV.foreach(path, :headers => true) do |row|
+      csv('landmarks.csv').each do |row|
 
         lon = row['Lon'].to_f
         lat = row['Lat'].to_f
@@ -19,7 +15,6 @@ module Import
         )
 
       end
-
     end
 
     def down
