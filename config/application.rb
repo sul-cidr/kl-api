f3fa57bf143ec69e378d631e050fa4885af59484
David McClure <davidwilliammcclure@gmail.com>
2016-02-11 14:45:46 -0800

    Rename the top-level app class and the database names to kl_*.

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require 'neo4j/railtie'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module KbApi
+module KLApi
   class Application < Rails::Application
 
     # Settings in config/environments/* take precedence over those specified here.
